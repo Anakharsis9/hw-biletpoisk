e0ca0fa2199fdb84caf155b74fc7c66b5addbf09
Dayana Ismailova <dayana530807@gmail.com>
2023-06-26 17:21:08 +0600

    add cinema select

--- src/components/FilterSearch/index.tsx
+++ src/components/FilterSearch/index.tsx
@@ -5,17 +5,31 @@ import { Select } from "../Base/Select";
 import style from "./FilterSearch.module.scss";
 
 export const FilterSearch = () => {
-  const [genre, setGenreValue] = useState("");
-  const handleGenreSelect = (value: string) => {
-    setGenreValue(value);
-  };
   const options = [
+    { title: "Не выбран", value: "-" },
     { title: "Боевик", value: "Боевик" },
     { title: "Комедия", value: "Комедия" },
     { title: "Фэнтези", value: "Фэнтези" },
     { title: "Ужасы", value: "Ужасы" }
   ];
+  const cinemas = [
+    { title: "Не выбран", value: "-" },
+    { title: "cinema1", value: "cinema1" },
+    { title: "cinema2", value: "cinema2" },
+    { title: "cinema3", value: "cinema3" },
+  ];
+
+  const [genre, setGenreValue] = useState("");
+  const handleGenreSelect = (value: string) => {
+    setGenreValue(value);
+  };
   const selectedGenre = options.find(item => item.value === genre);
+
+  const [cinema, setCinemaValue] = useState("");
+  const handleCinemaSelect = (value: string) => {
+    setCinemaValue(value);
+  };
+  const selectedCinema = cinemas.find(item => item.value === cinema);
 
   return (
     <div className={style["filter-wrapper"]}>
@@ -37,6 +51,15 @@ export const FilterSearch = () => {
             options={options}
             placeholder="Выберите жанр"
             onChange={handleGenreSelect}
+          />
+        </label>
+        <label className={style["input-label"]}>
+          Кинотеатр
+          <Select
+            selected={selectedCinema || null}
+            options={cinemas}
+            placeholder="Выберите кинотеатр"
+            onChange={handleCinemaSelect}
           />
         </label>
       </div>
